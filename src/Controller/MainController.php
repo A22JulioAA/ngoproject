4e7354e8ecea3e713caa06425c49848b9f34ceab
a22julioaa <a22julioaa@iessanclemente.net>
2024-03-03 20:08:39 +0100

    Flash messages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -172,5 +173,13 @@ class MainController extends AbstractController
             "title" => "Add Organization",
             "form" => $form
         ]);
+    }
+
+    #[Route("/send-flash-apply", name: "send_flash_apply")]
+    public function sendFlashApply(): RedirectResponse
+    {
+        $this->addFlash("success", "Your request has been sent successfully!");
+
+        return $this->redirectToRoute("homepage");
     }
 }
